11593e9b7bc800fe40c86a15888afec93221aad5
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-30 07:33:08 -0300

    refactoring variables

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = (props) => {
+  console.log("header props", props);
   return <h1>{props.course}</h1>;
 };
 const Parts = (props) => {
@@ -12,6 +13,7 @@ const Parts = (props) => {
 };
 
 const Content = (props) => {
+  console.log("content", props);
   return (
     <div>
       <Parts part={props.part1} exercise={props.exercises1} />
@@ -21,6 +23,7 @@ const Content = (props) => {
   );
 };
 const Total = (props) => {
+  console.log("Total", props);
   return (
     <p>
       Number of exercises{" "}
@@ -31,13 +34,18 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
-
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
   return (
     <div>
       <Header course={course} />
