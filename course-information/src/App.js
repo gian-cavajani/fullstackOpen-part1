211480bf1ab1560f89bbf3e13527f2a15ac1da5d
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-30 19:30:08 -0300

    setCounter

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 const App = () => {
   const [counter, setCounter] = useState(0);
-  const handleClick = () => {
-    console.log("clicked");
+  const increaseOne = () => {
+    setCounter(counter + 1);
+  };
+  const setToZero = () => {
+    setCounter(0);
   };
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={handleClick}>plus</button>
+      <button onClick={increaseOne}>plus</button>
+      <button onClick={setToZero}>reset</button>
       {/* <button onClick={()=> console.log("clicked")}>plus</button> */}
     </div>
   );
