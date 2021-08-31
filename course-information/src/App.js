4f08e50edfeb89652bdc7ddb5f6b280af2848018
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-31 14:21:51 -0300

    handling arrays

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,36 +1,29 @@
 import React, { useState } from "react";
 
 const App = () => {
-  // const [left, setLeft] = useState(0);
-  // const [right, setRight] = useState(0);
-  const [clicks, setClicks] = useState({
-    left: 0,
-    right: 0,
-  });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const handleLeft = () => {
-    const newClicks = {
-      ...clicks, //es importante que este primero el object destructuring, y luego la prop a modificar en la function.
-      left: clicks.left + 1,
-      // right: clicks.right,
-    };
-    setClicks(newClicks);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L")); //allClicks.push("L") would work but it could cause bugs, also it mutates the existing array,
+    // , which i dont want, rather i'd prefer to make a new instance of the previous array.
+    setLeft(left + 1);
   };
-  const handleRight = () => {
-    const newClicks = {
-      // left: clicks.left,
-      ...clicks, //pasa todo el objeto clicks a la variable newClicks, si se especifica una prop del objeto esta no se pasa.
-      right: clicks.right + 1, //it is forbidden to use, clicks.right++ in react
-    };
-    setClicks(newClicks);
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
   };
-  //in this case it would be easier to declare 2 usestate hooks, but sometimes how its done now(with complex state) is preferable.
+  console.log("data", allClicks);
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeft}>left</button>
-      <button onClick={handleRight}>right</button>
-      {clicks.right}
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(" ")}</p>
+      {/* it joins the array into a string with the elements separated with a blank-space */}
     </div>
   );
 };
