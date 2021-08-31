fb1ec5d383339d725c9fc115cb856411518eff7c
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-31 16:02:03 -0300

    exercise 1.7 finished

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
+  let all = good + neutral + bad;
   const handleGood = () => {
     setGood(good + 1);
   };
@@ -26,6 +26,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>average: {((good - bad) / all).toFixed(2)}</p>
+      <p>positive: {((good * 100) / all).toFixed(1)} %</p>
     </div>
   );
 };
