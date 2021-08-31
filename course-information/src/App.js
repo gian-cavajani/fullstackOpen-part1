5fb56185da9bc2c6fc262f2745d780bc7529ee0e
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-31 15:13:05 -0300

    functions that return functions :p

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,39 +1,28 @@
 import React, { useState } from "react";
 
-const History = (props) => {
-  const { allClicks } = props;
-  if (allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {allClicks.join(" ")}</div>;
-};
-
-const Button = (props) => {
-  const { text, click } = props;
-  return <button onClick={click}>{text}</button>;
-};
-
 const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
+  const setToValue = (newValue) => () => {
+    setValue(newValue);
   };
+  //function that returns functions
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
+  // ESTTA FUNCTION DE ABAJO ES LO MISMO QUE LA DE ARRIBA
+  // const setToValue = (newValue) => {
+  //   const value = () => {
+  //     setValue(newValue);
+  //   };
+  //   return value;
+  // };
+
   return (
     <div>
-      {left}
-      <Button click={handleLeftClick} text="left" />
-      <Button click={handleRightClick} text="right" />
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      {/* <button onClick={() => setValue(1000)}>thousand</button> , HACEN LO MISMO, Y ESTA NO PRECISA LA FUNCTION CALL A LA FUNCTION DENTRO DE LA FUNCTION */}
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   );
 };
