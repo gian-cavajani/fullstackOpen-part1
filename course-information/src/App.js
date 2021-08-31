8f40dac8ba4e4e1772d7e96ab9b8a55400e117c7
Gianluca Cavajani <cavajanig@gmail.com>
2021-08-31 13:45:17 -0300

    Finished part 1.c

--- course-information/src/App.js
+++ course-information/src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ text, click }) => <button onClick={click}>{text}</button>;
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increaseOne = () => {
-    setCounter(counter + 1);
-  };
-  const setToZero = () => {
-    setCounter(0);
-  };
+  const [counter, setCounter] = useState(0); //every time the usestate hook is used, causes the app component(in this case) to be rerendered
+  const increaseOne = () => setCounter(counter + 1);
+  const minusOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseOne}>plus</button>
-      <button onClick={setToZero}>reset</button>
-      {/* <button onClick={()=> console.log("clicked")}>plus</button> */}
+      <Display counter={counter} />
+      <Button text="plus" click={increaseOne} />
+      <Button text="minus" click={minusOne} />
+      <Button text="reset" click={setToZero} />
     </div>
   );
 };
